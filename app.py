922c0d4e332181bdb86ad52049329441631849a7
Kourie <maro.ben10@gmail.com>
2021-03-26 12:14:26 +1300

    db

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, g, request, redirect
+import sqlite3
+import os
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
